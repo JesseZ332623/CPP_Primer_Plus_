612cebcb304226edef20dfe27d1345e5cf6c3c27
Jesse <3191955858@qq.com>
2024-04-19 22:44:13 +0800

    活用 STL 算法的回文字符串检测

---
+++ exercise_questions/16_#_#/src/16_10_2.cpp
@@ -0,0 +1,131 @@
+#include <MyLib/myLogerDef.h>
+
+#include <cctype>
+#include <csignal>
+#include <limits>
+#include <algorithm>
+#include <string>
+
+/**
+ * @brief 强行结束程序需要清理 I/O 流，所以需要重设 CTRL + C 的行为。
+*/
+void endBehaviorHandle(int __signalIndex);
+
+/**
+ * @brief 判断传入字符是否为处数字字符和字母字符之外的字符（标点符号）
+*/
+bool isInterpunction(const unsigned char __ch);
+
+void endBehaviorHandle(int __signalIndex)
+{
+    switch(__signalIndex)
+    {
+        case SIGINT:
+            /*
+                做梦都没想到，signal 库居然不适配 C++ 标准 I/O，
+                换成 C 标准 I/O，一切都解决了。。
+            */
+            printf("\033[38;5;1mForcefully ending.\033[38;5;7m\n");
+            std::exit(EXIT_SUCCESS);
+            break;
+
+        default:
+            break;
+    }
+}
+
+bool isInterpunction(const unsigned char __ch)
+{
+    if (std::isalnum(__ch) || std::isalpha(__ch)) { return false; }
+
+    return true;
+}
+
+/**
+ * @brief 老题目，判断一个字符串是否为回文，但这次要活用 STL 算法。
+ * 
+ * @param __str 一个字符串的拷贝
+ * 
+ * @return 一个 bool 类型，表面是不是回文字符串
+*/
+bool isPalindromeString(std::string & __str);
+
+bool isPalindromeString(std::string & __str)
+{
+    using namespace MyLib::MyLoger;
+    /*
+        对字符串做两方面处理：
+        1. 全部转成小写
+        2. 处理掉所有标点符号
+    */
+    std::transform(
+                    __str.cbegin(), __str.cend(), __str.begin(), 
+                    [] (unsigned char n) { return std::tolower(n); }
+                  );
+
+    while (true)
+    {
+        std::string::iterator interpunctionIter = \
+        std::find_if(
+                        __str.begin(), __str.end(), 
+                        [](const unsigned char __ch) -> bool { return isInterpunction(__ch); }
+                    );
+
+        if (interpunctionIter == __str.end()) { break; }
+
+        __str.erase(interpunctionIter);  
+    }
+
+    /*
+        当经过上述两步处理后，字符串长度为 0，
+        很可能意味着用户只输入了标点符号，或者非法输入，所有也要返回 false
+    */
+    if (__str.size() == 0) 
+    {
+        loger(std::cout, WARNING, "Invald Input!\n");
+        return false;
+    }
+
+    std::string reverseString; reverseString.resize(__str.size());
+    std::reverse_copy(__str.cbegin(), __str.cend(), reverseString.begin());
+
+    if (__str == reverseString) { return true; }
+
+    return false;
+}
+
+int main(int argc, char const *argv[])
+{
+    using namespace MyLib::MyLoger;
+
+    system("cls");
+
+    /*
+        注册信号函数，指定 CTRL + C 的行为。
+    */
+    std::signal(SIGINT, endBehaviorHandle);
+
+    loger(std::cout, NOTIFY, "This Program Check The String You Input Is PalindromeString or Not.\n");
+    
+    std::string userInput;
+
+    loger(std::cout, NOTIFY, "Please enter something: ");
+
+    while (std::getline(std::cin, userInput) && (userInput != ".quit"))
+    {
+        if (isPalindromeString(userInput))
+        {
+            loger(std::cout, CORRECT, "String: [", userInput, "] is PalindromeString!\n");
+        }
+        else
+        {
+            loger(std::cout, WARNING, "String: [", userInput, "] is not a PalindromeString!\n");
+        }
+
+        loger(std::cout, NOTIFY, "Please enter something again: \n");
+    }
+
+    DONE
+
+    return EXIT_SUCCESS;
+}
